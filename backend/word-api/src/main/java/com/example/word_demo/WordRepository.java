dedab44c3a347088edf4831043203a4389422af1
jwetjens <117168292+jwetjens@users.noreply.github.com>
2025-04-10 16:31:35 +0200

    Update backend for new figma designs

--- backend/word-api/src/main/java/com/example/word_demo/WordRepository.java
+++ backend/word-api/src/main/java/com/example/word_demo/WordRepository.java
@@ -1,9 +1,16 @@
 package com.example.word_demo;
 
-import com.example.word_demo.Word;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 //Used to interact with the database
 
 public interface WordRepository extends JpaRepository<Word, Long> {
+
+    @Query(value = "SELECT * FROM word LIMIT :limit OFFSET :skip", nativeQuery = true)
+    List<Word> fetchWordsWithOffset(@Param("skip") int skip, @Param("limit") int limit);
+
 }
