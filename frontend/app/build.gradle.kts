81e5754e13f121a0f6c7b8de4aa07ea8ffeb1251
jwetjens <117168292+jwetjens@users.noreply.github.com>
2025-05-01 15:28:29 +0200

    Fixed issue

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
+    implementation("com.google.guava:guava:32.1.2-jre")
+
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
