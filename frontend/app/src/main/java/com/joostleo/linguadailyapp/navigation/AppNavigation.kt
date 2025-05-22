ca50f437c980331c95dd129dbbacd26b0b7f6d87
Tozzettini <lziche@gmail.com>
2025-05-22 21:07:48 +0200

    Boarding Bombocvlaat screen bomvbo omg

--- frontend/app/src/main/java/com/joostleo/linguadailyapp/navigation/AppNavigation.kt
+++ frontend/app/src/main/java/com/joostleo/linguadailyapp/navigation/AppNavigation.kt
@@ -1,24 +1,40 @@
 package com.joostleo.linguadailyapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.joostleo.linguadailyapp.ui.screens.BookmarkScreen
 import com.joostleo.linguadailyapp.ui.screens.DailyChallengeScreen
 import com.joostleo.linguadailyapp.ui.screens.HomeScreen
+import com.joostleo.linguadailyapp.ui.screens.OnboardingScreen
 import com.joostleo.linguadailyapp.ui.screens.SettingsScreen
 import com.joostleo.linguadailyapp.ui.screens.WordDetailScreen
 import com.joostleo.linguadailyapp.ui.screens.WordsScreen
+import com.joostleo.linguadailyapp.utils.preferences.PreferencesManager
+
 
 class AppNavigation {
 
     @Composable
-    fun start() {
+    fun start(
+
+    ) {
+        val context = LocalContext.current
+        val prefs = PreferencesManager(context)
+
+
         val navController = rememberNavController()
 
+        val startDestination = if (prefs.isOnboardingCompleted()) {
+            NavigationDestinations.Home.route
+        } else {
+            NavigationDestinations.Onboarding.route
+        }
+
         // Define NavHost with Start Destination
-        NavHost(navController = navController, startDestination = NavigationDestinations.Home.route) {
+        NavHost(navController = navController, startDestination = startDestination) {
             composable(NavigationDestinations.Home.route) { HomeScreen(navController) }
             composable(NavigationDestinations.Settings.route) { SettingsScreen(navController) }
             composable(NavigationDestinations.WordsList.route) { WordsScreen(navController) }
@@ -30,6 +46,8 @@ class AppNavigation {
                     WordDetailScreen(navController, id)
                 }
             }
+            composable(NavigationDestinations.Onboarding.route) { OnboardingScreen(navController) }
+
 
         }
     }
--- frontend/app/src/main/java/com/joostleo/linguadailyapp/navigation/NavigationDestinations.kt
+++ frontend/app/src/main/java/com/joostleo/linguadailyapp/navigation/NavigationDestinations.kt
@@ -6,6 +6,7 @@ sealed class NavigationDestinations(val route: String) {
     object WordsList : NavigationDestinations("wordsList")
     object Bookmark : NavigationDestinations("bookMark")
     object Challenge : NavigationDestinations("challenge")
+    object Onboarding : NavigationDestinations("onboarding")
     object Word : NavigationDestinations("word/{id}") {
         fun createRoute(id: Int): String = "word/$id"
     }
